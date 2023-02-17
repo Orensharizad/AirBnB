881b33908c950ccebfebf25cb7ccec9df9bbaade
Oren Sharizad <oren.sharizad12@gmail.com>
2023-02-17 14:45:59 +0200

    add stays data to the stay index

---
+++ src/app/models/stay-model.ts
@@ -0,0 +1,7 @@
+export interface Stay {
+    _id: string
+    name: string
+    type:string
+    imgUrls: string[]
+    price : number
+}
--- src/app/views/stay-index/stay-index.component.ts
+++ src/app/views/stay-index/stay-index.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Stay } from 'src/app/models/stay-model';
 
 @Component({
   selector: 'stay-index',
@@ -6,5 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./stay-index.component.scss']
 })
 export class StayIndexComponent {
+
+  stays:Stay[] = [
+    {
+      "_id": 's101',
+      "name": "Westin Kaanapali KORVN 2BR",
+      "type": "National parks",
+      "imgUrls": [
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436975/hx9ravtjop3uqv4giupt.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436294/mvhb3iazpiar6duvy9we.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436496/ihozxprafjzuhil9qhh4.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436952/aef9ajipinpjhkley1e3.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436948/vgfxpvmcpd2q40qxtuv3.jpg"
+      ],
+      "price": 595,
+    },
+    {
+      "_id": 's102',
+
+      "name": "Westin Kaanapali KORVN 2BR",
+      "type": "National parks",
+      "imgUrls": [
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436975/hx9ravtjop3uqv4giupt.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436294/mvhb3iazpiar6duvy9we.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436496/ihozxprafjzuhil9qhh4.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436952/aef9ajipinpjhkley1e3.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436948/vgfxpvmcpd2q40qxtuv3.jpg"
+      ],
+      "price": 595,
+    },
+    {
+      "_id": 's103',
+
+      "name": "Westin Kaanapali KORVN 2BR",
+      "type": "National parks",
+      "imgUrls": [
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436975/hx9ravtjop3uqv4giupt.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436294/mvhb3iazpiar6duvy9we.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436496/ihozxprafjzuhil9qhh4.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436952/aef9ajipinpjhkley1e3.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436948/vgfxpvmcpd2q40qxtuv3.jpg"
+      ],
+      "price": 595,
+    },
+    {
+      "_id": 's104',
+
+      "name": "Westin Kaanapali KORVN 2BR",
+      "type": "National parks",
+      "imgUrls": [
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436975/hx9ravtjop3uqv4giupt.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436294/mvhb3iazpiar6duvy9we.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436496/ihozxprafjzuhil9qhh4.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436952/aef9ajipinpjhkley1e3.jpg",
+        "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436948/vgfxpvmcpd2q40qxtuv3.jpg"
+      ],
+      "price": 595,
+    },
+  ]
 
 }
