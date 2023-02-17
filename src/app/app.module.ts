908da69884683078611c3056463d1424635f5052
liad7 <liadgola7@gmail.com>
2023-02-17 14:31:32 +0200

    basic stay cmps stracture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app-root/app.component';
+import { StayIndexComponent } from './views/stay-index/stay-index.component';
+import { StayListComponent } from './cmps/stay-list/stay-list.component';
+import { StayPreviewComponent } from './cmps/stay-preview/stay-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StayIndexComponent,
+    StayListComponent,
+    StayPreviewComponent
   ],
   imports: [
     BrowserModule,
