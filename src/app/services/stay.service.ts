3bddea2db4875fcf3389536d37290d72336da8fd
liad7 <liadgola7@gmail.com>
2023-02-19 00:36:00 +0200

    add util service

--- src/app/services/stay.service.ts
+++ src/app/services/stay.service.ts
@@ -1,30 +1,20 @@
 import { Injectable } from '@angular/core'
 import { Stay } from 'src/app/models/stay-model'
+import { UtilService } from './util.service'
 import data from '../../data.json'
 @Injectable({
   providedIn: 'root'
 })
 export class StayService {
-
-  constructor() { }
+  constructor(private utilService: UtilService) { }
+  // constructor() { }
   KEY = 'stayBD'
   getStays(): Stay[] {
-    let stays = this.loadFromStorage(this.KEY)
+    let stays = this.utilService.loadFromStorage(this.KEY)
     if (!stays) {
       stays = data
-      this.saveToStorage(this.KEY, stays)
-      // this.saveToStorage(this.KEY, data)
-      // stays = this.loadFromStorage(this.KEY)
+      this.utilService.saveToStorage(this.KEY, stays)
     }
     return stays
-  }
-
-  saveToStorage(key: string, value: any) {
-    localStorage.setItem(key, JSON.stringify(value))
-  }
-
-  loadFromStorage(key: string): any {
-    const data = localStorage.getItem(key)
-    return (data) ? JSON.parse(data) : undefined
   }
 }
--- src/app/views/stay-index/stay-index.component.ts
+++ src/app/views/stay-index/stay-index.component.ts
@@ -17,17 +17,3 @@ export class StayIndexComponent implements OnInit {
     this.getStays()
   }
 }
-
-// {
-//   "_id": 's101',
-//   "name": "Westin Kaanapali KORVN 2BR",
-//   "type": "National parks",
-//   "imgUrls": [
-//     "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436975/hx9ravtjop3uqv4giupt.jpg",
-//     "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436294/mvhb3iazpiar6duvy9we.jpg",
-//     "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436496/ihozxprafjzuhil9qhh4.jpg",
-//     "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436952/aef9ajipinpjhkley1e3.jpg",
-//     "http://res.cloudinary.com/dmtlr2viw/image/upload/v1663436948/vgfxpvmcpd2q40qxtuv3.jpg"
-//   ],
-//   "price": 595,
-// }
