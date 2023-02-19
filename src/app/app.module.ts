76ed852123d645a4e1c6d837383e89729a69730e
Oren Sharizad <oren.sharizad12@gmail.com>
2023-02-19 19:00:55 +0200

    start working on stay-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StayListComponent } from './cmps/stay-list/stay-list.component';
 import { StayPreviewComponent } from './cmps/stay-preview/stay-preview.component';
 import { AppHeaderComponent } from './cmps/app-header/app-header.component';
 import { ToolBarComponent } from './cmps/tool-bar/tool-bar.component';
+import { StayDetailsComponent } from './views/stay-details/stay-details.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ToolBarComponent } from './cmps/tool-bar/tool-bar.component';
     StayListComponent,
     StayPreviewComponent,
     AppHeaderComponent,
-    ToolBarComponent
+    ToolBarComponent,
+    StayDetailsComponent
   ],
   imports: [
     BrowserModule,
