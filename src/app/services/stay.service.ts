613083231032a82930c35b5ec83c60e4d58db0f8
liad7 <liadgola7@gmail.com>
2023-02-19 16:20:42 +0200

    query with observable

--- src/app/services/stay.service.ts
+++ src/app/services/stay.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core'
 import { Stay } from 'src/app/models/stay-model'
 import { UtilService } from './util.service'
 import data from '../../data.json'
+import { BehaviorSubject } from 'rxjs'
 @Injectable({
   providedIn: 'root'
 })
 export class StayService {
   constructor(private utilService: UtilService) { }
-  // constructor() { }
+
   KEY = 'stayBD'
-  getStays(): Stay[] {
+
+  private _stays$ = new BehaviorSubject<Stay[]>([]);
+  public stays$ = this._stays$.asObservable()
+
+  public query() {
     let stays = this.utilService.loadFromStorage(this.KEY)
     if (!stays) {
       stays = data
       this.utilService.saveToStorage(this.KEY, stays)
     }
-    return stays
+    this._stays$.next(stays)
   }
 }
