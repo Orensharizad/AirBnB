a07a75e40f75c64fd85dac5c2cff2948af686d72
Oren Sharizad <oren.sharizad12@gmail.com>
2023-02-22 17:40:59 +0200

    deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: environment.prodaction })],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
