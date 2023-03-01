4071aa9fafff9462e101a7a7f0cb5384610ba7ce
liad7 <liadgola7@gmail.com>
2023-03-01 15:02:07 +0200

    image upload func

--- src/app/views/stay-edit/stay-edit.component.ts
+++ src/app/views/stay-edit/stay-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { NgForm } from '@angular/forms'
+import { Map } from 'src/app/models/map-model'
 import { StayService } from 'src/app/services/stay.service'
 
 @Component({
@@ -9,20 +10,56 @@ import { StayService } from 'src/app/services/stay.service'
 })
 export class StayEditComponent {
   constructor(private stayService: StayService) { }
-  imgUrls: string[] = Array(5).fill('')
-  // numbers: number[] = Array(5).fill(0).map((x, i) => i)
+  numbers: number[] = Array(5).map((x, i) => i)
 
   onSubmit(form: NgForm) {
-    const amenities = []
-    const { amenitieMap } = form.value
-    for (const amenitie in amenitieMap) {
-      if (amenitieMap[amenitie]) amenities.push(amenitie)
-    }
-    const newStay = { ...form.value, amenities, imgUrl: this.imgUrls.filter(img => img) }
+    const { amenitieMap, imgUrlMap } = form.value
+    const amenities = this.objectToArray(amenitieMap)
+    let imgUrls = this.objectToValueArray(imgUrlMap)
+    imgUrls = imgUrls.map(async url=>{
+      return await this.uploadImg(url)
+    })
+    const newStay = { ...form.value, amenities, imgUrls }
     delete newStay.amenitieMap
+    delete newStay.imgUrlMap
     console.log('newStay:', newStay)
     // this.stayService.save(newStay)
     // form.reset()
+  }
+
+  objectToArray(obj: Map): string[] {
+    const arr = []
+    for (const key in obj) {
+      if (obj[key]) arr.push(key)
+    }
+    return arr
+  }
+
+  objectToValueArray(obj: Map): (string | undefined | Promise<any>)[] {
+    const arr = []
+    for (const key in obj) {
+      if (obj[key]) arr.push(obj[key])
+    }
+    return arr
+  }
+
+  uploadImg(file: any):Promise<any> {
+    console.log('file:', file);
+    const CLOUD_NAME = "dug85f2rg"
+    const UPLOAD_PRESET = "vspwyn8d"
+    const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+
+    const formData = new FormData()
+    formData.append('upload_preset', UPLOAD_PRESET)
+    formData.append('file', file)
+
+    return fetch(UPLOAD_URL, {
+      method: 'POST',
+      body: formData
+    })
+      .then(res => res.json())
+      .then(res => res.secure_url)
+      .catch(err => console.error(err))
   }
 
   amenities: string[] = [
@@ -81,5 +118,6 @@ export class StayEditComponent {
     'Step-free access',
     'Wide entryway',
     'Waterfront',
-    'Beachfront']
+    'Beachfront'
+  ]
 }
